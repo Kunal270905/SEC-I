f0ce5c2abb22e434df2c51c3f1f7d3aa19a753cf
Kunal <104131640+Kunal270905@users.noreply.github.com>
2025-09-08 12:15:59 +0530

    Add files via upload

---
+++ IncDecExample.java
@@ -0,0 +1,11 @@
+public class IncDecExample{
+    public static void main(String[] args){
+        int x=10;
+        System.out.println("Original:"+x);
+        System.out.println("Post-increment:"+(x++));
+        System.out.println("After Post-increment:"+x);
+        System.out.println("Pre-increment:"+(++x));
+        System.out.println("Post-decrement:"+(x--));
+        System.out.println("Pre-decrement:"+(--x));
+    }
+}
