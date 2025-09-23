18479ef54548d838829e08ca9833a6c0ad685d01
Kunal <104131640+Kunal270905@users.noreply.github.com>
2025-09-23 15:39:09 +0530

    Add files via upload

---
+++ ReturnExample.java
@@ -0,0 +1,15 @@
+public class ReturnExample{
+    public static vid main(String[] args){
+        Systemout.println("Start of program");
+        checknumber(5);
+        System.out.println("End of program");
+checkNumber finishes(return only exists checkNumber, not main)
+    }
+    public static void checkNumber(int num){
+        if(num<0){
+            System.out.println("Negative number");
+            return;
+        }
+        System.out.println("Positive number is zero");
+    }
+}
